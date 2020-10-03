93dd8523c22bff78d658b0d9540928d0959eded0
Adolfo Espinoza <ades_first@hotmail.com>
2020-10-03 12:44:57 -0500

    Uso de pipes

--- my-recipe-class02/src/app/countries/countries.component.ts
+++ my-recipe-class02/src/app/countries/countries.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import * as countries from '../../assets/countries.json';
 import { Country } from '@app/models';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
+  providers: [UpperCasePipe]
 })
 export class CountriesComponent implements OnInit {
 
   selected: Country;
+  birthday = new Date();
+  total = 1234567890;
 
-  constructor() {
-    this.selected = countries[7];
+  constructor(private upperCasePipe: UpperCasePipe) {
+    this.selected = countries[11];
     console.log(this.selected.name);
   }
 
   ngOnInit(): void {
   }
 
+  send(name: string): void{
+    console.log('Antes = ', name);
+    name = this.upperCasePipe.transform(name);
+    console.log('Despues = ', name);
+  }
 }
